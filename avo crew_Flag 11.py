2098ca3716ab65a0cea57e6eb00bdf68a9f8cdc0
jwtan <jwtan0402@gmail.com>
2024-10-07 13:48:53 +0800

    add solutions for 6 7 and 11

---
+++ avo crew_Flag 11.py
@@ -0,0 +1,20 @@
+potion_inventory = {
+    "Polyjuice Potion": "Frost Fern",
+    "Amortentia": "Fire Lily",
+    "Frostbite Elixir": "Phoenix Feather",
+    "Elixir of Life": "Aconite Root",
+    "Firestarter Potion":"Elderflower"
+}
+
+keys = list(potion_inventory.keys())
+values = list(potion_inventory.values())
+
+sorted_keys = sorted(keys)
+sorted_values = sorted(values)
+
+for i in range(len(sorted_keys)):
+    potion_inventory[sorted_keys[i]] = sorted_values[i]
+
+
+
+print(sorted(potion_inventory.values()))
---
+++ avo crew_Flag 7.py
@@ -0,0 +1,24 @@
+# 1. If the spell is found in the "spells" category, retrieve its "description".
+# 2. If the spell is found in the "common_spells" category, retrieve its "power".
+# 3. If a spell is not found in either category, it is ignored.
+
+spells = {"spells": 
+          {"fireball": {"description": "Burns with the fury of a thousand suns", "power": "High"},
+           "lightning": {"description": "Strikes with the speed of thunder", "power": "Medium"},
+            "bolt": {"description": "A quick and sharp surge", "power": "Low"}},
+        "common_spells": {"rays": {"description": "A blazing inferno","power": "High"},
+                        "explosion": {"description": "A sudden burst","power": "Low"}}}
+
+encrypted_message = "fireball lightning bolt rays explosion"
+encrypted_list = encrypted_message.split()
+decrypted = ""
+
+for spell_name in encrypted_list:
+    if spell_name in spells["spells"]:
+        decrypted += spells["spells"][spell_name]['description'] + " "
+    elif spell_name in spells["common_spells"]:
+        decrypted += spells['common_spells'][spell_name]['power'] + " "
+    else:
+        continue
+        
+print(decrypted)
